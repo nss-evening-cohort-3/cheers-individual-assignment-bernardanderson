be9c537aff0a0868525d35e845263f871a548087
bernardanderson <bernard.anderson@gmail.com>
2016-07-11 20:21:10 -0500

    Added for loop to loop a display letters in name

--- Program.cs
+++ Program.cs
@@ -13,6 +13,10 @@ namespace Cheers
             Console.Write("> ");
             String user_name = Console.ReadLine(); // Read from terminal to a variable
 
+            for (int i = 0; i <user_name.Length; i++) {
+                Console.WriteLine("Give me a.. " + user_name.ToLower()[i]);
+            }
+
             // user_name.ToUpper() is turning the user's name to uppercase.
             // ToUpper() is finctionality specific to strings.
             // ToUpper accepts no arguments
